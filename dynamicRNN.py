57bbd5d28f5ef7a0b888a98c1e73fcde15946f02
Ujjwal Gupta <ujjawal@ujjawals-MacBook-Pro.local>
2018-02-17 13:05:13 +0530

    Using dynamic RNN to find number of 1's in a list

---
+++ dynamicRNN.py
@@ -0,0 +1,107 @@
+import numpy as np
+import tensorflow as tf
+import random
+from random import shuffle
+
+seq_length = 10
+test_set_size = 10000
+#generating training data
+def generate_data(seq_length,training_set_size):
+	temp = []
+	#generating training sequences
+	while(training_set_size > 0):
+		s = ""
+		for i in range(seq_length):
+			s += str(int(round(random.random())))
+		temp.append(s)
+		training_set_size -= 1
+	input_list = []
+	label_list = []
+	#converting input data in (batch_size,seq_length(time_steps),input_size)
+	for i in temp:
+		temp_list = []
+		count = 0
+		all_zero = [0]*(seq_length+1)
+		for j in str(i):
+			temp_list.append([int(j)])
+			count += int(j)
+		input_list.append(temp_list)
+		all_zero[seq_length - count] = 1
+		label_list.append(all_zero)
+	return (input_list,label_list)
+
+
+#parameters
+#time steps through which the RNN is unrolled for
+time_steps = seq_length
+#Number of inputs at each time steps
+batch_size = 1000
+#Number of hidden layers
+num_layers = 1
+#number of LSTM units in each LSTM cell
+num_units = 128
+#size of input vector
+input_size = 1
+#output size
+output_size = seq_length + 1
+#learning rate
+learning_rate = 0.01
+#no. of iterations
+iterations = 500
+
+#Building the graph
+X = tf.placeholder("float",[None,time_steps,input_size])    #None represents the batch size
+Y = tf.placeholder("float",[None,output_size])
+
+#output weights and bias
+output_weights = tf.Variable(tf.random_normal([num_units,output_size]))
+output_bias = tf.Variable(tf.random_normal([output_size]))
+
+#input = tf.unstack(X ,time_steps,1)
+
+#Building the RNN
+hidden_layer = tf.contrib.rnn.BasicLSTMCell(num_units, forget_bias = 1)
+output,state = tf.nn.dynamic_rnn(hidden_layer,X,dtype = "float32")
+
+#prediction
+output = tf.transpose(output,[1,0,2])
+last_output = tf.gather(output,int(output.get_shape()[0]) - 1)
+pred = tf.matmul(last_output,output_weights) + output_bias
+real_pred = tf.nn.softmax(pred);
+
+#cost function
+cost = -tf.reduce_sum(Y * tf.log(tf.clip_by_value(real_pred,1e-10,1.0)))
+
+#optimizer
+optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cost)
+
+#evaluating the model
+correct_pred = tf.equal(tf.argmax(pred,1),tf.argmax(Y,1))
+in_sample_accuracy = tf.reduce_mean(tf.cast(correct_pred,tf.float32))
+
+#initialising the model
+model = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+	sess.run(model)
+	#training
+	while(iterations > 0):
+		train_x,train_y = generate_data(10,20)
+		sess.run(optimizer, feed_dict = {X : train_x, Y:train_y})
+		c = sess.run(cost, feed_dict = {X : train_x, Y:train_y})
+		if(iterations%100 == 0):
+			print(c)
+		iterations -= 1
+
+	#testing
+	test_x,test_y = generate_data(seq_length,test_set_size)
+	prediction = sess.run(real_pred,feed_dict = {X : test_x, Y : test_y})
+	out_sample_accuracy = sess.run(in_sample_accuracy, feed_dict = {X : test_x, Y : test_y})
+	print("prediction: ",prediction)
+	print("Accuracy: ", out_sample_accuracy)
+
+
+
+
+
+
