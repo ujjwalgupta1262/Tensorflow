b2945efd514aa565ffd58c6c99f10bc5c8880aca
Ujjwal Gupta <ujjwalgupta1262@gmail.com>
2018-03-09 12:25:53 +0530

    Adding new code for pointer networks

---
+++ pointer.py
@@ -0,0 +1,141 @@
+import tensorflow as tf
+from keras import backend as K
+from keras import regularizers, constraints, initializers, activations
+from keras.layers.recurrent import Recurrent
+from keras.engine import InputSpec
+from tdd import _time_distributed_dense
+
+class AttentionDecoder(Recurrent):
+
+    def __init__(self, units, output_dim,
+                 activation="tanh",
+                 name='AttentionDecoder',
+                 kernel_initializer='glorot_uniform',
+                 recurrent_initializer='orthogonal',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 **kwargs):
+
+        self.units = units
+        self.output_dim = output_dim
+        self.activation = activations.get("tanh")
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.recurrent_initializer = initializers.get(recurrent_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.recurrent_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.recurrent_constraint = constraints.get(kernel_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+
+        super(AttentionDecoder, self).__init__(**kwargs)
+        self.name = name
+        self.return_sequences = True  # must return sequences
+
+    def build(self, input_shape):
+
+        self.batch_size, self.timesteps, self.input_dim = input_shape
+
+        self.states = [None, None]  # y, s
+
+        self.V_a = self.add_weight(shape=(self.units,),
+                                   name='V_a',
+                                   initializer=self.kernel_initializer,
+                                   regularizer=self.kernel_regularizer,
+                                   constraint=self.kernel_constraint)
+
+        self.W_a = self.add_weight(shape=(self.units, self.units),
+                                   name='W_a',
+                                   initializer=self.kernel_initializer,
+                                   regularizer=self.kernel_regularizer,
+                                   constraint=self.kernel_constraint)
+        self.U_a = self.add_weight(shape=(self.input_dim, self.units),
+                                   name='U_a',
+                                   initializer=self.kernel_initializer,
+                                   regularizer=self.kernel_regularizer,
+                                   constraint=self.kernel_constraint)
+
+        self.b_a = self.add_weight(shape=(self.units,),
+                                   name='b_a',
+                                   initializer=self.bias_initializer,
+                                   regularizer=self.bias_regularizer,
+                                   constraint=self.bias_constraint)
+
+        # For creating the initial state:
+        self.W_s = self.add_weight(shape=(self.input_dim, self.units),
+                                   name='W_s',
+                                   initializer=self.recurrent_initializer,
+                                   regularizer=self.recurrent_regularizer,
+                                   constraint=self.recurrent_constraint)
+
+        self.input_spec = [
+            InputSpec(shape=(self.batch_size, self.timesteps, self.input_dim))]
+        self.built = True
+
+    def call(self, x):
+        # store the whole sequence so we can "attend" to it at each timestep
+        self.x_seq = x
+
+        # apply the a dense layer over the time dimension of the sequence
+        # do it here because it doesn't depend on any previous steps
+        # thefore we can save computation time:
+        self._uxpb = _time_distributed_dense(self.x_seq, self.U_a, b=self.b_a,
+                                             input_dim=self.input_dim,
+                                             timesteps=self.timesteps,
+                                             output_dim=self.units)
+
+        return super(AttentionDecoder, self).call(x)
+
+    def get_initial_state(self, inputs):
+        print('inputs shape:', inputs.get_shape())
+
+        # apply the matrix on the first time step to get the initial s0.
+        s0 = activations.tanh(K.dot(inputs[:, 0], self.W_s))
+
+        # from keras.layers.recurrent to initialize a vector of (batchsize,
+        # output_dim)
+        y0 = K.zeros_like(inputs)  # (samples, timesteps, input_dims)
+        y0 = K.sum(y0, axis=(1, 2))  # (samples, )
+        y0 = K.expand_dims(y0)  # (samples, 1)
+        y0 = K.tile(y0, [1, self.output_dim])
+
+        return [y0, s0]
+
+    def step(self, x, states):
+
+        ytm, stm = states
+
+        # repeat the hidden state to the length of the sequence
+        _stm = K.repeat(stm, self.timesteps)
+
+        # now multiplty the weight matrix with the repeated hidden state
+        _Wxstm = K.dot(_stm, self.W_a)
+
+        # calculate the attention probabilities
+        # this relates how much other timesteps contributed to this one.
+        et = K.dot(activations.tanh(_Wxstm + self._uxpb),
+                   K.expand_dims(self.V_a))
+        at = K.exp(et)
+        at_sum = K.sum(at, axis=1)
+        at_sum_repeated = K.repeat(at_sum, self.timesteps)
+        at /= at_sum_repeated  # vector of size (batchsize, timesteps, 1)
+
+        return at
+
+if __name__ == '__main__':
+    from keras.layers import Input, LSTM
+    from keras.models import Model
+    from keras.layers.wrappers import Bidirectional
+    i = Input(shape=(100,104), dtype='float32')
+    enc = Bidirectional(LSTM(64, return_sequences=True), merge_mode='concat')(i)
+    dec = AttentionDecoder(32, 4)(enc)
+    model = Model(inputs=i, outputs=dec)
+    model.summary()
